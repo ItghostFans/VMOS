b6c82a7ab1fa9dd0dcbd6657242977721831fce4
fanchunxing1 <fanchunxing1@joyy.com>
2024-12-03 19:32:08 +0800

    0.1.0

---
+++ ReleasePod.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#~/.cocoapods
+#pod spec edit ReactiveObjC
+
+tag=$1
+podspec=`ls *.podspec`
+podName=${podspec%.*}
+
+sed -i '' "s/\'[0-9]*\.[0-9]*\.[0-9]*.*\'/\'${tag}\'/g" "$podName.podspec"
+
+git tag -d $tag
+git push --delete origin $tag
+
+git add *
+git commit -m $tag
+git push -u origin main
+
+git tag $tag
+git push origin $tag
+git push origin --tags
+
+# 前置条件
+#pod repo list
+#pod trunk register ItghostFans@gmail.com ItghostFans --verbose
+
+# repo='cocoapods'
+pod trunk push $podName.podspec --allow-warnings --verbose
