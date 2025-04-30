fb286cf8a400643f8c3f53abe65d6f4b11b4e983
fanchunxing1 <fanchunxing1@joyy.com>
2025-04-30 23:00:23 +0800

    fix (Bug): 这里有个异常。

--- VMOS/Classes/Foundation/NSObject+Runtime.m
+++ VMOS/Classes/Foundation/NSObject+Runtime.m
@@ -14,9 +14,10 @@
 + (void)runtime_swizzleSel:(SEL)oldSel newSel:(SEL)newSel {
     Method oldMethod = class_getInstanceMethod(self, oldSel);
     Method newMethod = class_getInstanceMethod(self, newSel);
+    IMP oldMethodImp = method_getImplementation(oldMethod);
     IMP newMethodImp = method_getImplementation(newMethod);
     if (class_addMethod(self, oldSel, newMethodImp, method_getTypeEncoding(newMethod))) {
-        class_replaceMethod(self, oldSel, newMethodImp, method_getTypeEncoding(oldMethod));
+        class_replaceMethod(self, newSel, oldMethodImp, method_getTypeEncoding(oldMethod));
     } else {
         method_exchangeImplementations(oldMethod, newMethod);
     }
