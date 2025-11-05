9d1947e4fa5c5136cf2718343fc3774f6e7351a9
fanchunxing1 <fanchunxing1@joyy.com>
2025-11-05 11:45:53 +0800

    feat (ResizableLayer): 9宫格Layer。

---
+++ VMOS/Classes/QuartzCore/VMOSShapeGradientLayer.h
@@ -0,0 +1,16 @@
+//
+//  VMOSShapeGradientLayer.h
+//  Expecta
+//
+//  Created by ItghostFan on 2025/4/25.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface VMOSShapeGradientLayer : CAShapeLayer
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ VMOS/Classes/QuartzCore/VMOSShapeGradientLayer.m
@@ -0,0 +1,76 @@
+//
+//  VMOSShapeGradientLayer.m
+//  Expecta
+//
+//  Created by ItghostFan on 2025/4/25.
+//
+
+#import "VMOSShapeGradientLayer.h"
+
+#import <VMOS/NSObject+Runtime.h>
+
+@interface VMOSShapeGradientLayer ()
+@property (strong, nonatomic) NSMutableArray *colors;
+@property (assign, nonatomic) NSInteger index;
+@end
+
+@implementation VMOSShapeGradientLayer
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        [self runtime_swizzleSel:@selector(addAnimation:forKey:) newSel:@selector(vmk_addAnimation:forKey:)];
+    });
+}
+
+- (void)vmk_addAnimation:(CAAnimation *)anim forKey:(nullable NSString *)key {
+//    NSLog(@"Key %@ Animation %@", key.description, anim.debugDescription);
+    [self vmk_addAnimation:anim forKey:key];
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _colors = [NSMutableArray arrayWithObjects:UIColor.redColor, UIColor.blueColor, UIColor.yellowColor, UIColor.greenColor, nil];
+    }
+    return self;
+}
+
+//- (void)setFrame:(CGRect)frame {
+//    NSLog(@"Check Draw Time!");
+//    [super setFrame:frame];
+//}
+//
+- (void)drawInContext:(CGContextRef)ctx {
+    NSLog(@"Check Draw Time!");
+    [super drawInContext:ctx];
+}
+
+- (void)renderInContext:(CGContextRef)ctx {
+    NSLog(@"Check Render Time!");
+    [super renderInContext:ctx];
+}
+//
+//- (void)display {
+//    NSLog(@"Check Display Time!");
+//    [super display];
+//}
+
+- (instancetype)presentationLayer {
+    VMOSShapeGradientLayer *presentationLayer = [super presentationLayer];
+    UIBezierPath *path = [UIBezierPath bezierPathWithRect:presentationLayer.bounds];
+    path.lineWidth = 1.0f;
+    presentationLayer.path = path.CGPath;
+    NSLog(@"Check Presentation %@!", [presentationLayer debugDescription]);
+    return presentationLayer;
+}
+
+- (instancetype)modelLayer {
+    VMOSShapeGradientLayer *modelLayer = [super modelLayer];
+    UIBezierPath *path = [UIBezierPath bezierPathWithRect:modelLayer.bounds];
+    path.lineWidth = 1.0f;
+    modelLayer.path = path.CGPath;
+    NSLog(@"Check Model %@!", [modelLayer debugDescription]);
+    return modelLayer;
+}
+
+@end
